2d0fdf70911c5f03730ca72466e16751012fd5f6
Matt Gordon <bigmattsize21@gmail.com>
2020-05-01 02:01:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Black", "Cuddly", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Brown", "Crazy", 1);
+INSERT INTO bears (name) VALUES ("Mr. Chocolate");
+INSERT INTO bears (name) VALUES ("Rowdy");
 INSERT INTO bears (name) VALUES ("Tabita");
 INSERT INTO bears (name) VALUES ("Sergeant Brown");
 INSERT INTO bears (name) VALUES ("Melissa");
